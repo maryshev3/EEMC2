064cdaadd167ab94c8c96cb75ddb415b41c3c7f0
Марышев Андрей Владимирович <maryshev_av@rtsoft.ru>
2025-01-05 09:07:14 +0000

    Создал проект ВПФ

---
+++ EEMC2/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EEMC2.ViewModels
+{
+    class ViewModelLocator
+    {
+        public static ServiceProvider _serviceProvider;
+
+        public static void Init()
+        {
+            var services = new ServiceCollection();
+
+            services.AddSingleton<IServiceProvider, ServiceProvider>(_ => _serviceProvider);
+
+            _serviceProvider = services.BuildServiceProvider();
+        }
+    }
+}
