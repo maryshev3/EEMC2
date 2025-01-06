6aab935ebc15ff7d574808878980363af11feb48
Марышев Андрей Владимирович <maryshev_av@rtsoft.ru>
2025-01-06 12:53:48 +0000

    Добавил комманды

--- EEMC2/App.xaml.cs
+++ EEMC2/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿using EEMC2.ViewModels;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +10,12 @@ namespace EEMC2
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            ViewModelLocator.Init();
+
+            base.OnStartup(e);
+        }
     }
 
 }
