ceb8bc6981a69077d1b1e4992952890f67f10fb7
maryshev3 <maryshev3@gmail.com>
2025-01-25 11:11:54 +0000

    Сделал проект для генерации картинок

---
+++ EEMC2.ImageGenerator.Test/Program.cs
@@ -0,0 +1,5 @@
+﻿using EEMC2.ImageGenerator;
+
+var imageGenerator = new ImageGenerator(100, 100);
+
+imageGenerator.Generate("testFile4.jpeg");
---
+++ EEMC2.ImageGenerator/GradientFiller.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EEMC2.ImageGenerator
+{
+    public class GradientFiller
+    {
+        private readonly Color _startColor;
+        private readonly Color _endColor;
+        private readonly LinearGradientMode _linearGradientMode;
+
+        public GradientFiller(Color startColor, Color endColor, LinearGradientMode linearGradientMode) 
+        {
+            _startColor = startColor;
+            _endColor = endColor;
+            _linearGradientMode = linearGradientMode;
+        }
+
+        public void FillBitmap(Bitmap bitmap)
+        {
+            using Graphics graphics = Graphics.FromImage(bitmap);
+
+            Rectangle rectangle = new(
+                0,
+                0,
+                bitmap.Width,
+                bitmap.Height
+            );
+
+            using LinearGradientBrush brush = new LinearGradientBrush(
+                rectangle,
+                _startColor,
+                _endColor,
+                _linearGradientMode
+            );
+
+            graphics.FillRectangle(
+                brush,
+                rectangle
+            );
+        }
+    }
+}
