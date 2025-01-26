1e5fd82578d742f9c0f003bb2dde592c336d8a7f
Марышев Андрей Владимирович <maryshev_av@rtsoft.ru>
2025-01-26 09:23:02 +0000

    Реализовал метод CourseFullService.Get

--- EEMC2.Services/Services/CourseFull/CourseFullService.cs
+++ EEMC2.Services/Services/CourseFull/CourseFullService.cs
@@ -1,11 +1,15 @@
-﻿using EEMC2.Services.Services.Course;
+﻿using EEMC2.Services.Models;
+using EEMC2.Services.Services.Course;
 using EEMC2.Services.Services.CourseImage;
+using EEMC2.Services.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CourseFullFromModel = EEMC2.Services.Models.CourseFull;
+using CourseImageFromModel = EEMC2.Services.Models.CourseImage;
+using ImageGeneratorUtil = EEMC2.ImageGenerator.ImageGenerator;
 
 namespace EEMC2.Services.Services.CourseFull
 {
@@ -13,20 +17,36 @@ namespace EEMC2.Services.Services.CourseFull
     {
         private readonly ICourseService _courseService;
         private readonly ICourseImageService _courseImageService;
+        private readonly CourseImageGenerator _courseImageGenerator;
 
-        public CourseFullService(ICourseService courseService, ICourseImageService courseImageService)
+        public CourseFullService(
+            ICourseService courseService,
+            ICourseImageService courseImageService,
+            CourseImageGenerator courseImageGenerator
+        )
         {
             _courseService = courseService;
             _courseImageService = courseImageService;
+            _courseImageGenerator = courseImageGenerator;
         }
 
         public IEnumerable<CourseFullFromModel> Get()
         {
-            //var courses = _courseService.Get();
+            var courses = _courseService.Get();
 
-            //var courseImages = _courseImageService.Get();
+            var coursesIdToImage = _courseImageService
+                .Get()
+                .ToDictionary(x => x.CourseId);
 
-            throw new NotImplementedException();
+            var courseFulls = courses.Select(course => new CourseFullFromModel() 
+            {
+                Course = course,
+                CourseImage = coursesIdToImage.TryGetValue(course.Id, out CourseImageFromModel existingCourseImage)
+                    ? existingCourseImage
+                    : _courseImageGenerator.GenerateImageAndSave(course.Id)
+            }).ToList();
+
+            return courseFulls;
         }
     }
 }
--- EEMC2/ViewModels/ViewModelLocator.cs
+++ EEMC2/ViewModels/ViewModelLocator.cs
@@ -1,17 +1,24 @@
 ﻿using EEMC2.Services.Models;
 using EEMC2.Services.Repositories.Course;
 using EEMC2.Services.Services.Course;
+using EEMC2.Services.Services.CourseImage;
+using EEMC2.Services.Utils;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ImageGeneratorUtil = EEMC2.ImageGenerator.ImageGenerator;
 
 namespace EEMC2.ViewModels
 {
     class ViewModelLocator
     {
+        private const uint _imageWidth = 600;
+        private const uint _imageHeight = 356;
+
         public static ServiceProvider _serviceProvider;
 
         public static MainWindowVM MainWindowVM => _serviceProvider.GetService<MainWindowVM>();
@@ -36,8 +43,16 @@ namespace EEMC2.ViewModels
 
             services.AddSingleton<IServiceProvider, ServiceProvider>(_ => _serviceProvider);
 
+            services.AddSingleton<ImageGeneratorUtil>(_ => new ImageGeneratorUtil(_imageWidth, _imageHeight));
+
             InitFileRepositories(services);
             InitServices(services);
+            services.AddSingleton<CourseImageGenerator>(sp => new CourseImageGenerator(
+                    courseImageService: sp.GetService<ICourseImageService>(),
+                    imageGenerator: sp.GetService<ImageGeneratorUtil>(),
+                    imagesBasePath: Path.Join(Environment.CurrentDirectory, "Images")
+                )
+            );
 
             services.AddSingleton<MainWindowVM>();
             services.AddSingleton<CoursesListVM>();
